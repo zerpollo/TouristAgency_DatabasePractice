24d28d157fdf92ab2f044bd6fb1ee9a6a1d28ba5
zerpollo <93397349+zerpollo@users.noreply.github.com>
2023-04-11 22:12:25 +0300

    Add project files.

---
+++ TouristAgency_DatabasePractice/Form1.cs
@@ -0,0 +1,10 @@
+namespace TouristAgency_DatabasePractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
