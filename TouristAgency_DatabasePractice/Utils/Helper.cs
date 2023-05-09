65a54027d82dd94b529e89e3a75f56f49dcbf700
Ex0oleg <olegexo@gmail.com>
2023-05-09 22:16:15 +0300

    Added helper for connection strings

---
+++ TouristAgency_DatabasePractice/Utils/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+
+namespace Utils
+{
+    /// <summary>
+    /// Util class for connection strings 
+    /// </summary>
+    public static class Helper
+    {
+        /// <summary>
+        /// Function for getting connection string from app.config
+        /// </summary>
+        /// <param name="name">The name of connections string in app.config</param>
+        /// <returns>Returns string representing connection string</returns>
+        public static string CnnValue(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
