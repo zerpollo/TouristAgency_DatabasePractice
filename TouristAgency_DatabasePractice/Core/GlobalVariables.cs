29d6b52d392af7735a5c5c9a5e987a4f27b5b2d9
Ex0oleg <olegexo@gmail.com>
2023-05-21 19:14:56 +0300

    added global variables class

---
+++ TouristAgency_DatabasePractice/Core/GlobalVariables.cs
@@ -0,0 +1,15 @@
+﻿using ModelClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouristAgency_DatabasePractice.Core
+{
+    public class GlobalVariables
+    {
+        // should be only changed once when launching the program
+        public static Client CurrentClient;
+    }
+}
