3b3c451a076bce09408b5da332be4b362b25c57c
Ex0oleg <olegexo@gmail.com>
2023-05-21 19:27:04 +0300

    fixed my stupid ass check

--- TouristAgency_DatabasePractice/UserInfoWindow.cs
+++ TouristAgency_DatabasePractice/UserInfoWindow.cs
@@ -22,8 +22,8 @@ namespace TouristAgency_DatabasePractice
         private void SubmitButton_Click(object sender, EventArgs e)
         {
             // long-ass check there's a way to make it better but idk
-            if(string.IsNullOrEmpty(FNTextBox.Text) && string.IsNullOrEmpty(LNTextBox.Text) && string.IsNullOrEmpty(AddressTextBox.Text)
-            && string.IsNullOrEmpty(CountryTextBox.Text) && string.IsNullOrEmpty(PhoneNTextBox.Text))
+            if(!string.IsNullOrEmpty(FNTextBox.Text) && !string.IsNullOrEmpty(LNTextBox.Text) && !string.IsNullOrEmpty(AddressTextBox.Text)
+            && !string.IsNullOrEmpty(CountryTextBox.Text) && !string.IsNullOrEmpty(PhoneNTextBox.Text))
             {
                 // creating new user to put it in Global variables class
                 Client client = new Client(FNTextBox.Text, LNTextBox.Text, AddressTextBox.Text, CountryTextBox.Text, PhoneNTextBox.Text);
