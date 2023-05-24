bc25aab46765a7361990a5d7e860bdce4e544e27
Ex0oleg <olegexo@gmail.com>
2023-05-24 20:03:12 +0300

    fixed Activity model class

--- TouristAgency_DatabasePractice/ModelClasses/Activity.cs
+++ TouristAgency_DatabasePractice/ModelClasses/Activity.cs
@@ -9,8 +9,6 @@ namespace ModelClasses
     public class Activity
     {
         private int id;
-        private int dateTimeID;
-        private int languageID;
         private int duration;
         private int peopleQuantity;
         private int price;
@@ -19,8 +17,6 @@ namespace ModelClasses
         private string location;
 
         public int ID { get => id; set => id = value; }
-        public int DateTimeID { get => dateTimeID; set => dateTimeID = value; }
-        public int LanguageID { get => languageID; set => languageID = value; }
         public int Duration { get => duration; set => duration = value; }
         public int PeopleQuantity { get => peopleQuantity; set => peopleQuantity = value; }
         public int Price { get => price; set => price = value; }
@@ -30,8 +26,6 @@ namespace ModelClasses
 
         public Activity(int dateTimeID, int languageID, int duration, int peopleQuantity, int price, string name, string description, string location)
         {
-            this.dateTimeID = dateTimeID;
-            this.languageID = languageID;
             this.duration = duration;
             this.peopleQuantity = peopleQuantity;
             this.price = price;
