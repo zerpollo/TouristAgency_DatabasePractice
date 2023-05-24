9f248ff620c06ff671a4b64b2bcd78120644dc6a
zerpollo <93397349+zerpollo@users.noreply.github.com>
2023-05-24 19:43:55 +0300

    Added UserControl for Activity

--- TouristAgency_DatabasePractice/UserControls/ActivityControl.Designer.cs
+++ TouristAgency_DatabasePractice/UserControls/ActivityControl.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TouristAgency_DatabasePractice.UserControls
 {
-    partial class RestaurantControl
+    partial class ActivityControl
     {
         /// <summary> 
         /// Required designer variable.
@@ -31,19 +31,17 @@
             this.panel2 = new System.Windows.Forms.Panel();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.label13 = new System.Windows.Forms.Label();
             this.PriceLabel = new System.Windows.Forms.Label();
-            this.CloseTimeLabel = new System.Windows.Forms.Label();
             this.OpenTimeLabel = new System.Windows.Forms.Label();
             this.label17 = new System.Windows.Forms.Label();
             this.label18 = new System.Windows.Forms.Label();
             this.AddButton = new System.Windows.Forms.Button();
-            this.PhoneNumberLabel = new System.Windows.Forms.Label();
-            this.label20 = new System.Windows.Forms.Label();
             this.LocationLabel = new System.Windows.Forms.Label();
             this.label22 = new System.Windows.Forms.Label();
             this.NameLabel = new System.Windows.Forms.Label();
             this.label24 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DurationLabel = new System.Windows.Forms.Label();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -51,30 +49,28 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.BurlyWood;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.DurationLabel);
             this.panel2.Controls.Add(this.textBox1);
             this.panel2.Controls.Add(this.pictureBox2);
-            this.panel2.Controls.Add(this.label13);
             this.panel2.Controls.Add(this.PriceLabel);
-            this.panel2.Controls.Add(this.CloseTimeLabel);
             this.panel2.Controls.Add(this.OpenTimeLabel);
             this.panel2.Controls.Add(this.label17);
             this.panel2.Controls.Add(this.label18);
             this.panel2.Controls.Add(this.AddButton);
-            this.panel2.Controls.Add(this.PhoneNumberLabel);
-            this.panel2.Controls.Add(this.label20);
             this.panel2.Controls.Add(this.LocationLabel);
             this.panel2.Controls.Add(this.label22);
             this.panel2.Controls.Add(this.NameLabel);
             this.panel2.Controls.Add(this.label24);
-            this.panel2.Location = new System.Drawing.Point(5, 6);
+            this.panel2.Location = new System.Drawing.Point(5, 7);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(421, 273);
-            this.panel2.TabIndex = 28;
+            this.panel2.Size = new System.Drawing.Size(421, 263);
+            this.panel2.TabIndex = 29;
             // 
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.Color.Tan;
-            this.textBox1.Location = new System.Drawing.Point(11, 138);
+            this.textBox1.Location = new System.Drawing.Point(11, 131);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
@@ -90,42 +86,21 @@
             this.pictureBox2.TabIndex = 25;
             this.pictureBox2.TabStop = false;
             // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label13.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label13.Location = new System.Drawing.Point(162, 105);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(22, 30);
-            this.label13.TabIndex = 39;
-            this.label13.Text = "-";
-            // 
             // PriceLabel
             // 
             this.PriceLabel.AutoSize = true;
             this.PriceLabel.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.PriceLabel.Location = new System.Drawing.Point(118, 90);
+            this.PriceLabel.Location = new System.Drawing.Point(96, 65);
             this.PriceLabel.Name = "PriceLabel";
             this.PriceLabel.Size = new System.Drawing.Size(27, 15);
             this.PriceLabel.TabIndex = 38;
             this.PriceLabel.Text = "null";
             // 
-            // CloseTimeLabel
-            // 
-            this.CloseTimeLabel.AutoSize = true;
-            this.CloseTimeLabel.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.CloseTimeLabel.Location = new System.Drawing.Point(190, 115);
-            this.CloseTimeLabel.Name = "CloseTimeLabel";
-            this.CloseTimeLabel.Size = new System.Drawing.Size(27, 15);
-            this.CloseTimeLabel.TabIndex = 37;
-            this.CloseTimeLabel.Text = "null";
-            // 
             // OpenTimeLabel
             // 
             this.OpenTimeLabel.AutoSize = true;
             this.OpenTimeLabel.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.OpenTimeLabel.Location = new System.Drawing.Point(118, 115);
+            this.OpenTimeLabel.Location = new System.Drawing.Point(96, 108);
             this.OpenTimeLabel.Name = "OpenTimeLabel";
             this.OpenTimeLabel.Size = new System.Drawing.Size(27, 15);
             this.OpenTimeLabel.TabIndex = 36;
@@ -135,17 +110,17 @@
             // 
             this.label17.AutoSize = true;
             this.label17.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label17.Location = new System.Drawing.Point(11, 115);
+            this.label17.Location = new System.Drawing.Point(28, 108);
             this.label17.Name = "label17";
-            this.label17.Size = new System.Drawing.Size(106, 15);
+            this.label17.Size = new System.Drawing.Size(66, 15);
             this.label17.TabIndex = 35;
-            this.label17.Text = "Open - Close time:";
+            this.label17.Text = "Open time:";
             // 
             // label18
             // 
             this.label18.AutoSize = true;
             this.label18.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label18.Location = new System.Drawing.Point(79, 90);
+            this.label18.Location = new System.Drawing.Point(58, 65);
             this.label18.Name = "label18";
             this.label18.Size = new System.Drawing.Size(36, 15);
             this.label18.TabIndex = 34;
@@ -154,38 +129,18 @@
             // AddButton
             // 
             this.AddButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.AddButton.Location = new System.Drawing.Point(162, 224);
+            this.AddButton.Location = new System.Drawing.Point(162, 217);
             this.AddButton.Name = "AddButton";
             this.AddButton.Size = new System.Drawing.Size(97, 39);
             this.AddButton.TabIndex = 33;
             this.AddButton.Text = "Add";
             this.AddButton.UseVisualStyleBackColor = false;
             // 
-            // PhoneNumberLabel
-            // 
-            this.PhoneNumberLabel.AutoSize = true;
-            this.PhoneNumberLabel.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.PhoneNumberLabel.Location = new System.Drawing.Point(118, 65);
-            this.PhoneNumberLabel.Name = "PhoneNumberLabel";
-            this.PhoneNumberLabel.Size = new System.Drawing.Size(27, 15);
-            this.PhoneNumberLabel.TabIndex = 32;
-            this.PhoneNumberLabel.Text = "null";
-            // 
-            // label20
-            // 
-            this.label20.AutoSize = true;
-            this.label20.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label20.Location = new System.Drawing.Point(26, 65);
-            this.label20.Name = "label20";
-            this.label20.Size = new System.Drawing.Size(89, 15);
-            this.label20.TabIndex = 31;
-            this.label20.Text = "Phone number:";
-            // 
             // LocationLabel
             // 
             this.LocationLabel.AutoSize = true;
             this.LocationLabel.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.LocationLabel.Location = new System.Drawing.Point(118, 41);
+            this.LocationLabel.Location = new System.Drawing.Point(96, 41);
             this.LocationLabel.Name = "LocationLabel";
             this.LocationLabel.Size = new System.Drawing.Size(27, 15);
             this.LocationLabel.TabIndex = 30;
@@ -195,7 +150,7 @@
             // 
             this.label22.AutoSize = true;
             this.label22.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label22.Location = new System.Drawing.Point(59, 41);
+            this.label22.Location = new System.Drawing.Point(38, 41);
             this.label22.Name = "label22";
             this.label22.Size = new System.Drawing.Size(56, 15);
             this.label22.TabIndex = 29;
@@ -205,7 +160,7 @@
             // 
             this.NameLabel.AutoSize = true;
             this.NameLabel.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.NameLabel.Location = new System.Drawing.Point(118, 17);
+            this.NameLabel.Location = new System.Drawing.Point(96, 17);
             this.NameLabel.Name = "NameLabel";
             this.NameLabel.Size = new System.Drawing.Size(27, 15);
             this.NameLabel.TabIndex = 28;
@@ -215,20 +170,40 @@
             // 
             this.label24.AutoSize = true;
             this.label24.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label24.Location = new System.Drawing.Point(73, 17);
+            this.label24.Location = new System.Drawing.Point(52, 17);
             this.label24.Name = "label24";
             this.label24.Size = new System.Drawing.Size(42, 15);
             this.label24.TabIndex = 27;
             this.label24.Text = "Name:";
             // 
-            // RestaurantControl
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(96, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 15);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "null";
+            // 
+            // DurationLabel
+            // 
+            this.DurationLabel.AutoSize = true;
+            this.DurationLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.DurationLabel.Location = new System.Drawing.Point(38, 86);
+            this.DurationLabel.Name = "DurationLabel";
+            this.DurationLabel.Size = new System.Drawing.Size(56, 15);
+            this.DurationLabel.TabIndex = 41;
+            this.DurationLabel.Text = "Duration:";
+            // 
+            // ActivityControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(240)))), ((int)(((byte)(196)))));
             this.Controls.Add(this.panel2);
-            this.Name = "RestaurantControl";
-            this.Size = new System.Drawing.Size(432, 286);
+            this.Name = "ActivityControl";
+            this.Size = new System.Drawing.Size(433, 277);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
@@ -239,20 +214,18 @@
         #endregion
 
         private Panel panel2;
+        private Label label1;
+        private Label DurationLabel;
+        private TextBox textBox1;
         private PictureBox pictureBox2;
-        private Label label13;
         private Label PriceLabel;
-        private Label CloseTimeLabel;
         private Label OpenTimeLabel;
         private Label label17;
         private Label label18;
         private Button AddButton;
-        private Label PhoneNumberLabel;
-        private Label label20;
         private Label LocationLabel;
         private Label label22;
         private Label NameLabel;
         private Label label24;
-        private TextBox textBox1;
     }
 }
---
+++ TouristAgency_DatabasePractice/UserControls/ActivityControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TouristAgency_DatabasePractice.UserControls
+{
+    public partial class ActivityControl : UserControl
+    {
+        public ActivityControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
