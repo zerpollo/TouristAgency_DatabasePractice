c1fcb6589a5f34554c4c912c9bceaa9926404188
zerpollo <93397349+zerpollo@users.noreply.github.com>
2023-05-24 22:40:03 +0300

    Fixed ActivityControl, added combox selection for date and time, and language

--- TouristAgency_DatabasePractice/UserControls/ActivityControl.Designer.cs
+++ TouristAgency_DatabasePractice/UserControls/ActivityControl.Designer.cs
@@ -34,7 +34,6 @@
             this.DescriptionTextBox = new System.Windows.Forms.TextBox();
             this.pictureBox = new System.Windows.Forms.PictureBox();
             this.PriceLabel = new System.Windows.Forms.Label();
-            this.DateLabel = new System.Windows.Forms.Label();
             this.label17 = new System.Windows.Forms.Label();
             this.label18 = new System.Windows.Forms.Label();
             this.AddButton = new System.Windows.Forms.Button();
@@ -42,6 +41,9 @@
             this.label22 = new System.Windows.Forms.Label();
             this.NameLabel = new System.Windows.Forms.Label();
             this.label24 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DateTimeComboBox = new System.Windows.Forms.ComboBox();
+            this.LanguageComboBox = new System.Windows.Forms.ComboBox();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
@@ -49,12 +51,14 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.BurlyWood;
+            this.panel2.Controls.Add(this.LanguageComboBox);
+            this.panel2.Controls.Add(this.DateTimeComboBox);
+            this.panel2.Controls.Add(this.label1);
             this.panel2.Controls.Add(this.DurationLabel);
             this.panel2.Controls.Add(this.labell);
             this.panel2.Controls.Add(this.DescriptionTextBox);
             this.panel2.Controls.Add(this.pictureBox);
             this.panel2.Controls.Add(this.PriceLabel);
-            this.panel2.Controls.Add(this.DateLabel);
             this.panel2.Controls.Add(this.label17);
             this.panel2.Controls.Add(this.label18);
             this.panel2.Controls.Add(this.AddButton);
@@ -64,14 +68,14 @@
             this.panel2.Controls.Add(this.label24);
             this.panel2.Location = new System.Drawing.Point(5, 7);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(421, 263);
+            this.panel2.Size = new System.Drawing.Size(504, 293);
             this.panel2.TabIndex = 29;
             // 
             // DurationLabel
             // 
             this.DurationLabel.AutoSize = true;
             this.DurationLabel.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.DurationLabel.Location = new System.Drawing.Point(96, 86);
+            this.DurationLabel.Location = new System.Drawing.Point(315, 72);
             this.DurationLabel.Name = "DurationLabel";
             this.DurationLabel.Size = new System.Drawing.Size(27, 15);
             this.DurationLabel.TabIndex = 42;
@@ -81,7 +85,7 @@
             // 
             this.labell.AutoSize = true;
             this.labell.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.labell.Location = new System.Drawing.Point(38, 86);
+            this.labell.Location = new System.Drawing.Point(257, 72);
             this.labell.Name = "labell";
             this.labell.Size = new System.Drawing.Size(56, 15);
             this.labell.TabIndex = 41;
@@ -90,17 +94,17 @@
             // DescriptionTextBox
             // 
             this.DescriptionTextBox.BackColor = System.Drawing.Color.Tan;
-            this.DescriptionTextBox.Location = new System.Drawing.Point(11, 131);
+            this.DescriptionTextBox.Location = new System.Drawing.Point(11, 161);
             this.DescriptionTextBox.Multiline = true;
             this.DescriptionTextBox.Name = "DescriptionTextBox";
             this.DescriptionTextBox.ReadOnly = true;
-            this.DescriptionTextBox.Size = new System.Drawing.Size(392, 80);
+            this.DescriptionTextBox.Size = new System.Drawing.Size(482, 80);
             this.DescriptionTextBox.TabIndex = 40;
             // 
             // pictureBox
             // 
             this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox.Location = new System.Drawing.Point(295, 17);
+            this.pictureBox.Location = new System.Drawing.Point(385, 14);
             this.pictureBox.Name = "pictureBox";
             this.pictureBox.Size = new System.Drawing.Size(108, 108);
             this.pictureBox.TabIndex = 25;
@@ -110,37 +114,27 @@
             // 
             this.PriceLabel.AutoSize = true;
             this.PriceLabel.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.PriceLabel.Location = new System.Drawing.Point(96, 65);
+            this.PriceLabel.Location = new System.Drawing.Point(315, 25);
             this.PriceLabel.Name = "PriceLabel";
             this.PriceLabel.Size = new System.Drawing.Size(27, 15);
             this.PriceLabel.TabIndex = 38;
             this.PriceLabel.Text = "null";
             // 
-            // DateLabel
-            // 
-            this.DateLabel.AutoSize = true;
-            this.DateLabel.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.DateLabel.Location = new System.Drawing.Point(96, 108);
-            this.DateLabel.Name = "DateLabel";
-            this.DateLabel.Size = new System.Drawing.Size(27, 15);
-            this.DateLabel.TabIndex = 36;
-            this.DateLabel.Text = "null";
-            // 
             // label17
             // 
             this.label17.AutoSize = true;
             this.label17.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label17.Location = new System.Drawing.Point(60, 108);
+            this.label17.Location = new System.Drawing.Point(11, 135);
             this.label17.Name = "label17";
-            this.label17.Size = new System.Drawing.Size(34, 15);
+            this.label17.Size = new System.Drawing.Size(117, 15);
             this.label17.TabIndex = 35;
-            this.label17.Text = "Date:";
+            this.label17.Text = "Select date and time:";
             // 
             // label18
             // 
             this.label18.AutoSize = true;
             this.label18.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label18.Location = new System.Drawing.Point(58, 65);
+            this.label18.Location = new System.Drawing.Point(277, 25);
             this.label18.Name = "label18";
             this.label18.Size = new System.Drawing.Size(36, 15);
             this.label18.TabIndex = 34;
@@ -149,7 +143,7 @@
             // AddButton
             // 
             this.AddButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.AddButton.Location = new System.Drawing.Point(162, 217);
+            this.AddButton.Location = new System.Drawing.Point(216, 247);
             this.AddButton.Name = "AddButton";
             this.AddButton.Size = new System.Drawing.Size(97, 39);
             this.AddButton.TabIndex = 33;
@@ -160,7 +154,7 @@
             // 
             this.LocationLabel.AutoSize = true;
             this.LocationLabel.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.LocationLabel.Location = new System.Drawing.Point(96, 41);
+            this.LocationLabel.Location = new System.Drawing.Point(64, 75);
             this.LocationLabel.Name = "LocationLabel";
             this.LocationLabel.Size = new System.Drawing.Size(27, 15);
             this.LocationLabel.TabIndex = 30;
@@ -170,7 +164,7 @@
             // 
             this.label22.AutoSize = true;
             this.label22.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label22.Location = new System.Drawing.Point(38, 41);
+            this.label22.Location = new System.Drawing.Point(6, 75);
             this.label22.Name = "label22";
             this.label22.Size = new System.Drawing.Size(56, 15);
             this.label22.TabIndex = 29;
@@ -180,7 +174,7 @@
             // 
             this.NameLabel.AutoSize = true;
             this.NameLabel.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.NameLabel.Location = new System.Drawing.Point(96, 17);
+            this.NameLabel.Location = new System.Drawing.Point(64, 25);
             this.NameLabel.Name = "NameLabel";
             this.NameLabel.Size = new System.Drawing.Size(27, 15);
             this.NameLabel.TabIndex = 28;
@@ -190,11 +184,37 @@
             // 
             this.label24.AutoSize = true;
             this.label24.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label24.Location = new System.Drawing.Point(52, 17);
+            this.label24.Location = new System.Drawing.Point(20, 25);
             this.label24.Name = "label24";
             this.label24.Size = new System.Drawing.Size(42, 15);
             this.label24.TabIndex = 27;
             this.label24.Text = "Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(267, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 15);
+            this.label1.TabIndex = 43;
+            this.label1.Text = "Select language:";
+            // 
+            // DateTimeComboBox
+            // 
+            this.DateTimeComboBox.FormattingEnabled = true;
+            this.DateTimeComboBox.Location = new System.Drawing.Point(134, 132);
+            this.DateTimeComboBox.Name = "DateTimeComboBox";
+            this.DateTimeComboBox.Size = new System.Drawing.Size(127, 23);
+            this.DateTimeComboBox.TabIndex = 44;
+            // 
+            // LanguageComboBox
+            // 
+            this.LanguageComboBox.FormattingEnabled = true;
+            this.LanguageComboBox.Location = new System.Drawing.Point(366, 132);
+            this.LanguageComboBox.Name = "LanguageComboBox";
+            this.LanguageComboBox.Size = new System.Drawing.Size(127, 23);
+            this.LanguageComboBox.TabIndex = 45;
             // 
             // ActivityControl
             // 
@@ -203,7 +223,7 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(240)))), ((int)(((byte)(196)))));
             this.Controls.Add(this.panel2);
             this.Name = "ActivityControl";
-            this.Size = new System.Drawing.Size(433, 277);
+            this.Size = new System.Drawing.Size(516, 308);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
@@ -219,7 +239,6 @@
         private TextBox DescriptionTextBox;
         private PictureBox pictureBox;
         private Label PriceLabel;
-        private Label DateLabel;
         private Label label17;
         private Label label18;
         private Button AddButton;
@@ -227,5 +246,8 @@
         private Label label22;
         private Label NameLabel;
         private Label label24;
+        private Label label1;
+        private ComboBox LanguageComboBox;
+        private ComboBox DateTimeComboBox;
     }
 }
