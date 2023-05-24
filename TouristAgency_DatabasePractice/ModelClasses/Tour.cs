2fb1885a7c8daf2f083cec9c0d3b717cf64d787b
Ex0oleg <olegexo@gmail.com>
2023-05-24 20:07:21 +0300

    fixed tour model

--- TouristAgency_DatabasePractice/ModelClasses/Tour.cs
+++ TouristAgency_DatabasePractice/ModelClasses/Tour.cs
@@ -11,17 +11,20 @@ namespace ModelClasses
         private int id;
         private int clientID;
         private int price;
+        private int peopleQuantity;
         private DateTime issuanceDate;
 
         public int ID { get => id; set => id = value; }
         public int ClientID { get => clientID; set => clientID = value; }
         public int Price { get => price; set => price = value; }
         public DateTime IssuanceDate { get => issuanceDate; set => issuanceDate = value; }
+        public int PeopleQuantity { get => peopleQuantity; set => peopleQuantity = value; }
 
-        public Tour(int clientID, int price, DateTime issuanceDate)
+        public Tour(int clientID, int price, DateTime issuanceDate, int peopleQuantity)
         {
             this.ClientID = clientID;
             this.Price = price;
+            this.PeopleQuantity = peopleQuantity;
             this.IssuanceDate = issuanceDate;
         }
         public Tour()
