1d72744fad9c607819bf0563abb645dbb6d46f27
Ex0oleg <olegexo@gmail.com>
2023-05-25 19:00:53 +0300

    added class for standart values

---
+++ TouristAgency_DatabasePractice/Core/StandartValues.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouristAgency_DatabasePractice.Core
+{
+    public class StandartValues
+    {
+        public static List<TimeOnly> timeOptions = new List<TimeOnly>()
+        {
+            new TimeOnly(8,0),
+            new TimeOnly(9,0),
+            new TimeOnly(10,0),
+            new TimeOnly(11,0),
+            new TimeOnly(12,0),
+            new TimeOnly(13,0),
+            new TimeOnly(14,0),
+            new TimeOnly(15,0),
+            new TimeOnly(16,0),
+            new TimeOnly(17,0),
+            new TimeOnly(18,0),
+            new TimeOnly(19,0),
+            new TimeOnly(20,0),
+        };
+    }
+}
