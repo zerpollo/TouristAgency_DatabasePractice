643607291137b91d30d316873c8c3c4f438933c0
Ex0oleg <olegexo@gmail.com>
2023-05-26 00:13:24 +0300

    modified standart value class to reflect model class changes

--- TouristAgency_DatabasePractice/Core/StandartValues.cs
+++ TouristAgency_DatabasePractice/Core/StandartValues.cs
@@ -8,21 +8,22 @@ namespace TouristAgency_DatabasePractice.Core
 {
     public class StandartValues
     {
-        public static List<TimeOnly> timeOptions = new List<TimeOnly>()
+        public static List<TimeSpan> timeOptions = new List<TimeSpan>()
         {
-            new TimeOnly(8,0),
-            new TimeOnly(9,0),
-            new TimeOnly(10,0),
-            new TimeOnly(11,0),
-            new TimeOnly(12,0),
-            new TimeOnly(13,0),
-            new TimeOnly(14,0),
-            new TimeOnly(15,0),
-            new TimeOnly(16,0),
-            new TimeOnly(17,0),
-            new TimeOnly(18,0),
-            new TimeOnly(19,0),
-            new TimeOnly(20,0),
+             new TimeSpan(8,0,0),
+             new TimeSpan(9,0,0),
+             new TimeSpan(10,0,0),
+             new TimeSpan(11,0,0),
+             new TimeSpan(12,0,0),
+             new TimeSpan(13,0,0),
+             new TimeSpan(14,0,0),
+             new TimeSpan(15,0,0),
+             new TimeSpan(16,0,0),
+             new TimeSpan(17,0,0),
+             new TimeSpan(18,0,0),
+             new TimeSpan(19,0,0),
+             new TimeSpan(20,0,0),
         };
+
     }
 }
