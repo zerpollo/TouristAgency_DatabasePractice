429b05299016e44a9ac063d540efa8a2a6d129f4
Ex0oleg <olegexo@gmail.com>
2023-05-27 01:53:39 +0300

    added logic for displaying dates

--- TouristAgency_DatabasePractice/UserControls/ActivityControl.cs
+++ TouristAgency_DatabasePractice/UserControls/ActivityControl.cs
@@ -18,6 +18,7 @@ namespace TouristAgency_DatabasePractice.UserControls
         public Activity ActivityModel;
 
         private List<Language> availableLanguages = new List<Language>();
+        private List<DateTimeDB> availableTimes = new List<DateTimeDB>();
 
         public ActivityControl(Activity activity)
         {
@@ -39,17 +40,26 @@ namespace TouristAgency_DatabasePractice.UserControls
             DurationLabel.Text = activity.Duration.ToString();
             DescriptionTextBox.Text = activity.Description;
 
-           // availableLanguages = GlobalVariables.Languages.Where<>
            List<LanguageActivities> tempLang = GlobalVariables.LanguageActivities.Where(activitylang => activitylang.ActivityID == activity.ID).ToList();
 
            var IenumLanguages = from al in tempLang join lang in GlobalVariables.Languages on al.LanguageID equals lang.ID where al.ActivityID == activity.ID select lang;
 
            availableLanguages = IenumLanguages.ToList();
 
+            // setting datetime combobox
+            
+            List<ActivitiesDateTime> tempdates = GlobalVariables.activitiesDateTimes.Where(activityTime => activityTime.ActivityID == activity.ID).ToList();
+
+            var IenumDates = from ad in tempdates join dates in GlobalVariables.DateTimes on ad.DateTimeID equals dates.ID where ad.ActivityID == activity.ID select dates;
+
+            availableTimes = IenumDates.ToList();
 
             // fill drop downs
             LanguageComboBox.DataSource = availableLanguages;
             LanguageComboBox.DisplayMember = "Name";
+
+            DateTimeComboBox.DataSource = availableTimes;
+            DateTimeComboBox.DisplayMember = "DateTime";
     }
 
         private void AddButton_Click(object sender, EventArgs e)
