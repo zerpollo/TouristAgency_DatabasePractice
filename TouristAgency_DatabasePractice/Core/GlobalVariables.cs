1b28217ee39c6e497169c550d4c04ce41916549c
Ex0oleg <olegexo@gmail.com>
2023-05-26 17:09:49 +0300

    added lists for holding info from db and user selected items

--- TouristAgency_DatabasePractice/Core/GlobalVariables.cs
+++ TouristAgency_DatabasePractice/Core/GlobalVariables.cs
@@ -1,9 +1,12 @@
 ﻿using ModelClasses;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TouristAgency_DatabasePractice.ModelClasses.Junction;
+using Activity = ModelClasses.Activity;
 
 namespace TouristAgency_DatabasePractice.Core
 {
@@ -11,5 +14,22 @@ namespace TouristAgency_DatabasePractice.Core
     {
         // should be only changed once when launching the program
         public static Client CurrentClient;
+
+        // lists of everything we have in DB
+        public static List<Museum> Museums;
+        public static List<Restaraunt> Restaraunts;
+        public static List<Shop> Shops;
+        public static List<Activity> Activities;
+
+        // junction tables lists for Times and Languages available
+        public static List<LanguageActivities> LanguageActivities;
+        public static List<ActivitiesDateTime> activitiesDateTimes;
+
+        // Selected items
+        public static List<Museum> SelectedMuseums = new List<Museum>();
+        public static List<Restaraunt> SelectedRestaraunts = new List<Restaraunt>();
+        public static List<Shop> SelectedShops = new List<Shop>();
+        public static List<Activity> SelectedActivities = new List<Activity>();
+
     }
 }
