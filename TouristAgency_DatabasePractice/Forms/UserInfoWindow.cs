f5b24444a3212a6a6190d09b72e6c979dbaab86e
Ex0oleg <olegexo@gmail.com>
2023-05-27 12:22:10 +0300

    uploading current client and tour to the DB and getting it back to get IDs

--- TouristAgency_DatabasePractice/Forms/UserInfoWindow.cs
+++ TouristAgency_DatabasePractice/Forms/UserInfoWindow.cs
@@ -1,4 +1,5 @@
-﻿using ModelClasses;
+﻿using DataAcessUtils;
+using ModelClasses;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -20,7 +21,7 @@ namespace TouristAgency_DatabasePractice
             InitializeComponent();
         }
 
-        private void SubmitButton_Click(object sender, EventArgs e)
+        private async void SubmitButton_Click(object sender, EventArgs e)
         {
             // long-ass check there's a way to make it better but idk
             if(!string.IsNullOrEmpty(FNTextBox.Text) && !string.IsNullOrEmpty(LNTextBox.Text) && !string.IsNullOrEmpty(AddressTextBox.Text)
@@ -29,6 +30,21 @@ namespace TouristAgency_DatabasePractice
                 // creating new user to put it in Global variables class
                 Client client = new Client(FNTextBox.Text, LNTextBox.Text, AddressTextBox.Text, CountryTextBox.Text, PhoneNTextBox.Text);
                 GlobalVariables.CurrentClient = client;
+
+                // sending new client to db and getting it back
+                DataAccess da = new DataAccess();
+                await da.SaveData("dbo.InsertNewClientProc", new { client.FirstName, client.LastName, client.Address, client.Country, client.PhoneNumber });
+                Task<IEnumerable<Client>> TaskClient = da.LoadData<Client, dynamic>("dbo.GetClientByPhoneNumberProc", new { client.PhoneNumber });
+                IEnumerable<Client> IClient = await TaskClient;
+                GlobalVariables.CurrentClient = IClient.First();
+
+                // Creating new Tour and getting it back
+                Tour tour = new Tour();
+                tour.ClientID = GlobalVariables.CurrentClient.ID;
+                await da.SaveData("dbo.InsertNewTourProc", new { tour.ClientID });
+                Task<IEnumerable<Tour>> TaskTour = da.LoadData<Tour, dynamic>("dbo.GetTourByClientIDProc", new { tour.ClientID });
+                IEnumerable<Tour> ITour = await TaskTour;
+                GlobalVariables.CurrentTour = ITour.First();
 
                 // opening main window
                 MainWindow mainwindow = new MainWindow();
